86aa605294450981d7ce67d065cbfffd4f4f6500
ShaswatSrivastava <shaswat24678@gmail.com>
2026-06-03 21:11:45 +0530

    Implement longest subarray with sum K function

--- LongestSubarrayWithSumK.java
+++ LongestSubarrayWithSumK.java
@@ -1,3 +1,26 @@
-public class LongestSubarrayWithSumK {
+import java.util.HashMap;
 
+public class LongestSubarrayWithSumK {
+    public static int longestSubarray(int[] arr, int k) {
+        // code here
+        int ans = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+            if(map.containsKey(sum-k)){
+                ans = Math.max(ans, i - map.get(sum-k));
+            }
+            map.putIfAbsent(sum, i);
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 5, 2, 7, 1, -10};
+        int k = 15;
+        System.out.println(longestSubarray(arr, k));
+    }
 }
